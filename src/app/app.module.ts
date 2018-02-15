dcbc67532ac8096113abf6a1cfd447327bae55af
jadiago <jadiago@cpi.com.ph>
2018-02-15 13:39:08 +0800

    Angular Project Tutorial  with SASS Bootstrap Template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ExploreComponent } from './explore/explore.component';
+import { CreateComponent } from './create/create.component';
+import { ShareComponent } from './share/share.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ExploreComponent,
+    CreateComponent,
+    ShareComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
